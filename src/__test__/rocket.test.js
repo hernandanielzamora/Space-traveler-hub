d74bab2648a42ec20ff09d2afe95b755f51769db
haftamudesta <desta451616@hotmail.com>
2023-05-17 22:31:21 +0300

    Fix linter error

--- src/__test__/rocket.test.js
+++ src/__test__/rocket.test.js
@@ -1,14 +1,14 @@
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import Rockets from '../components/rockets/Rockets';
-import store from '../redux/Store';
+import store from '../redux/store';
 
 it('Test RocketContainer renders correctly', () => {
   const tree = renderer
     .create(
       <Provider store={store}>
         <Rockets />
-      </Provider>,
+      </Provider>
     )
     .toJSON();
   expect(tree).toMatchSnapshot();
